3559ba3754aa552571258f3b57481a5c99de88f9
ranjini <rranjinibr@gmail.com>
2024-09-09 12:58:05 +0530

    this is my first commit

---
+++ lab3.java
@@ -0,0 +1,5 @@
+public class student{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("welcome to java class");
+}
---
+++ lab4.py
@@ -0,0 +1,8 @@
+#include<stio.h>
+void main()
+{
+int a;
+int b;
+println("enter a and b");
+}
+return 0;
